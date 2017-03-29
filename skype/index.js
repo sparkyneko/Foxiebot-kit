2df55e328eb2f0f9b414075d0443be2563c71b88
Charlie Kobayashi <sparkychildcharlie@gmail.com>
2017-03-29 09:31:16 -0400

    Update index.js

--- skype/index.js
+++ skype/index.js
@@ -4,6 +4,7 @@ const Skyweb = require("skyweb");
 const fs = require("fs");
 
 const skypeAccount = Config.skypeAccount || null;
+let errorListener;
 
 global.SkypeBot = require("./bot");
 
@@ -11,38 +12,50 @@ log("info", "Initiating skype...");
 // connect to Skype!
 global.skyweb = new Skyweb();
 
-function login () {
+function initiate () {
     if (!skypeAccount) return log("error", "No skype account found - deprecating skype feature.");
     skyweb.login(skypeAccount.name, skypeAccount.pass).then(account => {
         log("ok", "Logged in to skype as: " + account._selfInfo.displayname);
     });
+    
+    // friend requests?
+    skyweb.authRequestCallback = function (requests) {
+        requests.forEach(request => {
+            skyweb.acceptAuthRequest(request.sender);
+            skyweb.sendMessage("8:" + request.sender, "I accepted you!");
+            log("request", "Accepted: " + request.sender);
+        });
+    };
+
+    skyweb.messagesCallback = function (messages) {
+        messages.forEach(message => {
+            if (message.resource.from.indexOf(skypeAccount.name) === -1 && message.resource.messagetype !== "Control/Typing" && message.resource.messagetype !== "Control/ClearTyping") {
+                SkypeBot.receive(message);
+            }
+        });
+    };
+    
+    errorListener = (eventName, err) => {
+        log("error", err);
+        if (err.includes("Failed to send message") || err.includes("Failed to poll message")) {
+            log("info", "Trying to login to skype again...");
+            global.skyweb = new Skyweb();
+            reinitiate(); // try logging in again.
+        }
+    };
+    skyweb.on('error', errorListener); // Adding error listener 
 }
 
-// friend requests?
-skyweb.authRequestCallback = function (requests) {
-    requests.forEach(request => {
-        skyweb.acceptAuthRequest(request.sender);
-        skyweb.sendMessage("8:" + request.sender, "I accepted you!");
-        log("request", "Accepted: " + request.sender);
-    });
-};
+function reinitiate() {
+    // run the death sequence
+    // remove the 2 old "listeners"
+    skyweb.messagesCallback = null;
+    skyweb.authRequestCallback = null;
+    
+    skyweb.un("error", errorListener);
+    delete global.skyweb;
+    
+    initiate(); // a new skyweb is reborn
+}
 
-skyweb.messagesCallback = function (messages) {
-    messages.forEach(message => {
-        if (message.resource.from.indexOf(skypeAccount.name) === -1 && message.resource.messagetype !== "Control/Typing" && message.resource.messagetype !== "Control/ClearTyping") {
-            SkypeBot.receive(message);
-        }
-    });
-};
-
-const errorListener = (eventName, err) => {
-    log("error", err);
-    if (err.includes("Failed to send message") || err.includes("Failed to poll message")) {
-        log("info", "Trying to login to skype again...");
-        global.skyweb = new Skyweb();
-        login(); // try logging in again.
-    }
-};
-skyweb.on('error', errorListener); // Adding error listener 
-
-login();
+initiate();
