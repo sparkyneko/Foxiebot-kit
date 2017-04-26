62d96fe4b6146cc39a95159e269283eaad633c89
sparkyfoxie <sparkyfoxie@gmail.com>
2017-04-26 04:08:02 +0000

    Add quotes module

---
+++ chat-plugins/quotes.js
@@ -0,0 +1,58 @@
+"use strict";
+
+Config.settableCommands.quote = true;
+
+exports.commands = {
+    q: "quote",
+    quote: function (target, room, user) {
+        if (!this.can("quotes")) return false;
+        let quotes = Object.keys(Db("quotes").get(room.id, {}));
+        if (!quotes.length) return false;
+        
+        let id = parseInt(target);
+        
+        let quote = id && quotes[id - 1] ? quotes[id - 1] : quotes[Math.floor(Math.random() * quotes.length)];
+        
+        this.send(quote);
+    },
+    
+    addquote: function (target, room, user) {
+        if (!this.can("quotes")) return false;
+        
+        if (Db("quotes").has([room.id, target])) return this.send("The quote already exists.");
+        
+        Db("quotes").set([room.id, target], 1);
+        this.send("Added a new quote.");
+    },
+    
+    deletequote: "delquote",
+    delquote: function (target, room, user) {
+        if (!this.can("quotes")) return false;
+        
+        if (parseInt(target)) {
+            let id = parseInt(target);
+            let quotes = Object.keys(Db("quotes").get(room.id, {}));
+            if (!quotes[id - 1]) return this.send("Invalid quote ID.");
+            
+            target = quotes[id - 1];
+        }
+        
+        if (!Db("quotes").has([room.id, target])) return this.send("The quote does not exists.");
+        
+        Db("quotes").delete([room.id, target]);
+        this.send("Deleted the quote.");
+    },
+    
+    quotes: function (target, room, user) {
+        if (!room) {
+            if (!Rooms.rooms.has(toId(target, true))) return this.send("Invalid room.");
+            room = Rooms.get(target);
+        }
+        
+        if (!user.can("quote", room)) return false;
+        let quotes = Object.keys(Db("quotes").get(room.id, {}))
+            .map((q, i) => (i + 1) + ". " + q);
+        
+        Tools.uploadToHastebin(quotes.join("\n\n"), link => this.send("Quotes for " + room.name + ": " + link));
+    }
+}
