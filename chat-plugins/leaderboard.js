91f4a7e75530674e4db2ff2c82923abd63bc3900
sparkyfoxie <sparkyfoxie@gmail.com>
2017-05-02 22:43:29 +0000

    Refactor Leaderboard to its own file

--- chat-plugins/leaderboard.js
+++ chat-plugins/leaderboard.js
@@ -3,24 +3,20 @@
 const DEFAULT_WIN_AMOUNT = 5;
 const FILE_DIRECTORY = "config/leaderboard.json";
 const Graph = require("../graph");
+const LEADERBOARD = require("../leaderboard");
 
 Config.settableCommands.managegames = true;
 
-let leaderboard = {};
-
-try {
-    leaderboard = JSON.parse(fs.readFileSync(FILE_DIRECTORY));
-} catch(e) { log("error", "MALFORMED JSON in leaderboard") }
-
-global.Leaderboard = {
-    settings: leaderboard.settings || {},
-    data: leaderboard.data || {},
+class MainLeaderboard extends LEADERBOARD {
+    constructor(file) {
+        super(file);
+    }
     
-    getGames: function () {
+    getGames() {
         return Object.keys(Monitor.games).filter(g => Monitor.games[g] === g);
-    },
+    }
     
-    onConfig: function (room, game, points) {
+    onConfig(room, game, points) {
         if (game === "constructor" || !(game in Monitor.games) || Monitor.games[game] !== game) return "Invalid game. Valid games are - " + this.getGames().join(", ") + ".";
         points = parseInt(points);
         if (!points || points < 1) return "Invalid point amount.";
@@ -28,78 +24,23 @@ global.Leaderboard = {
         if (!this.settings[room.id]) this.settings[room.id] = {};
         this.settings[room.id][game] = points;
         return this;
-    },
+    }
     
-    write: function () {
-		fs.writeFile(FILE_DIRECTORY, JSON.stringify({data: this.data, settings: this.settings}), err => {
-			if (err) console.log(`ERROR: Failed to write leaderboard - ${err}`);
-		});
-    },
-    
-    getWin: function (room, game) {
+    getWin(room, game) {
         if (!this.settings[room.id]) this.settings[room.id] = {};
         let points = this.settings[room.id][game] || DEFAULT_WIN_AMOUNT;
         
         return points;
-    },
+    }
     
-    onWin: function (game, room, userid, multiplier) {
+    onWin(game, room, userid, multiplier) {
         let points = this.getWin(room, game);
         
         return this.givePoints(room, userid, points * (Number(multiplier) || 1));
-    },
-    
-    getPoints: function (room, userid) {
-        if (!this.data[room.id]) this.data[room.id] = {};
-        
-        return this.data[room.id][userid] || 0;
-    },
-    
-    givePoints: function (room, userid, points) {
-        points = parseInt(points);
-        if (!points) return;
-        
-        if (!this.data[room.id]) this.data[room.id] = {};
-        if (!this.data[room.id][userid]) this.data[room.id][userid] = 0;
-        
-        this.data[room.id][userid] += points;
-        return this;
-    },
-    
-    visualiseLadder: function (room, userid) {
-        if (!userid) {
-            return Graph(this.data[room.id] || {}, {
-                showPlacement: true,
-                sort: "value",
-                title: `Leaderboard for '${room.name}'`,
-            });
-        } else {
-            return new Promise((resolve, reject) => {
-                let lowestScore = Infinity;
-                let lastPlacement = 1;
-                
-                let data = Object.assign({}, this.data[room.id] || {});
-                let result = Object.keys(data)
-                    .sort((a, b) => data[b] - data[a])
-                    .map((u, i) => {
-                        let d = data[u];
-                        if (d !== lowestScore) {
-                            lowestScore = d;
-                            lastPlacement = i + 1;
-                        }
-                        
-                        return {
-                            place: lastPlacement,
-                            userid: u,
-                            score: d,
-                        };
-                    }).find(u => u.userid === userid);
-                if (!result) return resolve(`'${userid}' does not have any points on the leaderboard for '${room.name}'.`);
-                resolve(`'${result.userid}' is #${result.place} on the leaderboard for '${room.name}' with ${result.score} points`);
-            });
-        }
     }
-};
+}
+
+global.Leaderboard = new MainLeaderboard(FILE_DIRECTORY);
 
 exports.commands = {
     games: function (target, room, user) {
---
+++ leaderboard.js
@@ -0,0 +1,78 @@
+class Leaderboard {
+    constructor(file) {
+        this.settings = {};
+        this.data = {};
+        
+        this.file = file;
+        
+        this.load();
+    }
+    
+    load() {
+        let leaderboard = {};
+        try {
+            leaderboard = JSON.parse(fs.readFileSync(this.file));
+        } catch(e) { log("error", "MALFORMED JSON in leaderboard") }
+        this.settings = leaderboard.settings || {};
+        this.data = leaderboard.data || {};
+    }
+    
+    write() {
+		fs.writeFile(this.file, JSON.stringify({data: this.data, settings: this.settings}), err => {
+			if (err) console.log(`ERROR: Failed to write leaderboard - ${err}`);
+		});
+    }
+    
+    getPoints(room, userid) {
+        if (!this.data[room.id]) this.data[room.id] = {};
+        
+        return this.data[room.id][userid] || 0;
+    }
+    
+    givePoints(room, userid, points) {
+        points = parseInt(points);
+        if (!points) return;
+        
+        if (!this.data[room.id]) this.data[room.id] = {};
+        if (!this.data[room.id][userid]) this.data[room.id][userid] = 0;
+        
+        this.data[room.id][userid] += points;
+        return this;
+    }
+    
+    visualiseLadder(room, userid) {
+        if (!userid) {
+            return Graph(this.data[room.id] || {}, {
+                showPlacement: true,
+                sort: "value",
+                title: `Leaderboard for '${room.name}'`,
+            });
+        } else {
+            return new Promise((resolve, reject) => {
+                let lowestScore = Infinity;
+                let lastPlacement = 1;
+                
+                let data = Object.assign({}, this.data[room.id] || {});
+                let result = Object.keys(data)
+                    .sort((a, b) => data[b] - data[a])
+                    .map((u, i) => {
+                        let d = data[u];
+                        if (d !== lowestScore) {
+                            lowestScore = d;
+                            lastPlacement = i + 1;
+                        }
+                        
+                        return {
+                            place: lastPlacement,
+                            userid: u,
+                            score: d,
+                        };
+                    }).find(u => u.userid === userid);
+                if (!result) return resolve(`'${userid}' does not have any points on the leaderboard for '${room.name}'.`);
+                resolve(`'${result.userid}' is #${result.place} on the leaderboard for '${room.name}' with ${result.score} points`);
+            });
+        }
+    }
+}
+
+module.exports = Leaderboard;
